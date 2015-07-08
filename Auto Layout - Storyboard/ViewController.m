32c7b3fbbda349e0e2cf55e4667fb2c44ad41262
TylerJJYan <tylerjjyan@gmail.com>
2015-07-08 11:19:47 -0700

    First Commit

---
+++ Auto Layout - Storyboard/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Auto Layout - Storyboard
+//
+//  Created by Tyler Yan on 2015-07-08.
+//  Copyright (c) 2015 Foodee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
